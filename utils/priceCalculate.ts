5b664c661a67ed7a1293d8bef20aa6e34d47bd25
Amel <53909124+ADEV-00@users.noreply.github.com>
2022-09-24 21:27:53 +0000

    Added home group calculate logic

--- utils/priceCalculate.ts
+++ utils/priceCalculate.ts
@@ -1,17 +1,27 @@
-export const tarifGroupHome = [
-  {
-    tarif: 1,
-    name: "Mjerno mjesto",
-    price: 4.8,
-  },
-  {
-    tarif: 1,
-    name: "Aktivna energija",
-    price: 0.1346,
-  },
-  {
-    tarif: 1,
-    name: "Obnovljivi izvori",
-    price: 0.0042,
-  },
-];
+const mjernoMjestoDomacinstva = 4.8;
+
+export const homeGroupFirstTarif = (aktivnaEnergija: number) => {
+  if (aktivnaEnergija) {
+    const obnovljiviIzvori = aktivnaEnergija * 0.0042;
+    const result = mjernoMjestoDomacinstva + aktivnaEnergija + obnovljiviIzvori;
+    console.log(Number(result).toFixed(2));
+    return Number(result).toFixed(2);
+  }
+  return;
+};
+
+export const homeGroupSecoundTarif = (
+  aktivnaEnergijaVeca: number,
+  aktivnaEnergijaManja: number
+): number => {
+  const obnovljiviIzvori =
+    (aktivnaEnergijaVeca + aktivnaEnergijaManja) * 0.0042;
+  const aktivnaEnergijaVecaTarifa = aktivnaEnergijaVeca * 0.1682;
+  const aktivnaEnergijaManjaTarifa = aktivnaEnergijaManja * 0.0841;
+  const result =
+    mjernoMjestoDomacinstva +
+    obnovljiviIzvori +
+    aktivnaEnergijaVecaTarifa +
+    aktivnaEnergijaManjaTarifa;
+  return Number(result.toFixed(2));
+};
