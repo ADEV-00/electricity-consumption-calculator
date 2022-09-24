b28c1290e1792092cbc07b31cc4d28b677b653e1
Amel <53909124+ADEV-00@users.noreply.github.com>
2022-09-24 10:20:24 +0000

    working on price calculate

---
+++ utils/priceCalculate.ts
@@ -0,0 +1,17 @@
+export const tarifGroupHome = [
+  {
+    tarif: 1,
+    name: "Mjerno mjesto",
+    price: 4.8,
+  },
+  {
+    tarif: 1,
+    name: "Aktivna energija",
+    price: 0.1346,
+  },
+  {
+    tarif: 1,
+    name: "Obnovljivi izvori",
+    price: 0.0042,
+  },
+];
