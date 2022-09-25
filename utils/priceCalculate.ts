149c4d45f4a60b4d18d8ce37cecd75fcf7aa3020
Amel <53909124+ADEV-00@users.noreply.github.com>
2022-09-25 14:58:28 +0000

    added first and secound tarif calculation for other group

--- utils/priceCalculate.ts
+++ utils/priceCalculate.ts
@@ -26,3 +26,45 @@ export const homeGroupSecoundTarif = (
     aktivnaEnergijaManjaTarifa;
   return Number(result.toFixed(2));
 };
+
+export const otherGroupFirstTarif = (
+  obracunSnage: number,
+  aktivnaEnergijaVecaTarifa: number,
+  aktivnaEnergijaManjaTarifa: number,
+  prekomjernaReaktivnaEnergija: number
+): number => {
+  const mjernoMjesto = 9.65;
+  const obracunSnageIznos = obracunSnage * 16.52;
+  const aktivnaEnergijaVecaTarifaIznos = aktivnaEnergijaVecaTarifa * 0.1489;
+  const aktivnaEnergijaManjaTarifaIzons = aktivnaEnergijaManjaTarifa * 0.0745;
+  const prekomjernaReaktivnaEnergijaIznos =
+    prekomjernaReaktivnaEnergija * 0.0322;
+  const obnovljiviIzvori =
+    (aktivnaEnergijaVecaTarifa + aktivnaEnergijaManjaTarifa) * 0.0042;
+  const result =
+    mjernoMjesto +
+    obracunSnageIznos +
+    aktivnaEnergijaVecaTarifaIznos +
+    aktivnaEnergijaManjaTarifaIzons +
+    prekomjernaReaktivnaEnergijaIznos +
+    obnovljiviIzvori;
+  return Number(result.toFixed(2));
+};
+
+export const otherGroupSecoundTarif = (
+  aktivnaEnergijaVecaTarifa: number,
+  aktivnaEnergijaManjaTarifa: number
+): number => {
+  const mjernoMjesto = 6.9;
+  const aktivnaEnergijaVecaTarifaIznos = aktivnaEnergijaVecaTarifa * 0.2391;
+  const aktvinaEnergijaManjaTarifaIzons = aktivnaEnergijaManjaTarifa * 0.1195;
+  const obnovljiviIzvori =
+    (aktivnaEnergijaVecaTarifa + aktivnaEnergijaManjaTarifa) * 0.0042;
+  const result =
+    mjernoMjesto +
+    aktivnaEnergijaVecaTarifaIznos +
+    aktvinaEnergijaManjaTarifaIzons +
+    obnovljiviIzvori;
+
+  return Number(result.toFixed(2));
+};
